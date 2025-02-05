7647c5df220fde83a10221d80542a4e3499f9d7f
baberldn <berilbadan1903@gmail.com>
2025-02-05 03:55:22 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./App.css"
 import Home from './pages/Home'
 import Register from './pages/Register'
